e85cf62caffd071e59fadfe9dd79c4ae56670e57
Roman Fedorchenko <s19312@pjwstk.edu.pl>
2020-05-29 13:33:20 +0200

    commit

---
+++ tut11/tut11/Controllers/HelloWorldController1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tut11.Controllers
+{
+    public class HelloWorldController1 : Controller
+    {
+        [HttpGet("/HelloWorld/")]
+        public IActionResult Index()
+        {
+            return Ok("This is a defaul action...");
+        }
+
+        [HttpGet("/HelloWorld/Welcome/")]
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            return Ok(HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}"));
+        }
+    }
+}
